6577f68c04b819e0169d14d96f2a5de1aa8c432b
Kalpana. M <kalpanaramakrishna25@gmail.com>
2026-07-05 09:56:10 +0530

    Create Subtraction.java

---
+++ Subtraction.java
@@ -0,0 +1,11 @@
+public class Subtraction {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+        int difference = a - b;
+
+        System.out.println("First Number: " + a);
+        System.out.println("Second Number: " + b);
+        System.out.println("Difference = " + difference);
+    }
+}
