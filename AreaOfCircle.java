4dbc708b5d58d19d225b8b7159f7a38da2451090
Kalpana. M <kalpanaramakrishna25@gmail.com>
2026-07-05 10:11:29 +0530

    Create AreaOfCircle.java

---
+++ AreaOfCircle.java
@@ -0,0 +1,9 @@
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        double radius = 7;
+        double area = 3.14 * radius * radius;
+
+        System.out.println("Radius = " + radius);
+        System.out.println("Area of Circle = " + area);
+    }
+}
