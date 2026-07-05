a7eec64fe8f069b761ae73961f40685895022534
Kalpana. M <kalpanaramakrishna25@gmail.com>
2026-07-05 09:58:23 +0530

    Create LargestNumber.java

---
+++ LargestNumber.java
@@ -0,0 +1,15 @@
+public class LargestNumber {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 25;
+        int c = 15;
+
+        if (a >= b && a >= c) {
+            System.out.println(a + " is the largest number");
+        } else if (b >= a && b >= c) {
+            System.out.println(b + " is the largest number");
+        } else {
+            System.out.println(c + " is the largest number");
+        }
+    }
+}
