88e0dc2abdc3b672660e573a788d001172a331bc
Kalpana. M <kalpanaramakrishna25@gmail.com>
2026-07-05 09:54:56 +0530

    Create Addition.java

---
+++ Addition.java
@@ -0,0 +1,11 @@
+public class Addition {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+
+        System.out.println("First Number: " + a);
+        System.out.println("Second Number: " + b);
+        System.out.println("Sum = " + sum);
+    }
+}
