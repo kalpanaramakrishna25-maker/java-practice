5ee79553508d24e7e449e76995ffdcb483babb71
Kalpana. M <kalpanaramakrishna25@gmail.com>
2026-07-05 09:56:42 +0530

    Create Multiplication.java

---
+++ Multiplication.java
@@ -0,0 +1,11 @@
+public class Multiplication {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int product = a * b;
+
+        System.out.println("First Number: " + a);
+        System.out.println("Second Number: " + b);
+        System.out.println("Product = " + product);
+    }
+}
