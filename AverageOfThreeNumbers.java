278414726ba0f7175872dd33df9a48772090fdae
Kalpana. M <kalpanaramakrishna25@gmail.com>
2026-07-05 10:11:56 +0530

    Create AverageOfThreeNumbers.java

---
+++ AverageOfThreeNumbers.java
@@ -0,0 +1,14 @@
+public class AverageOfThreeNumbers {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        double average = (a + b + c) / 3.0;
+
+        System.out.println("First Number = " + a);
+        System.out.println("Second Number = " + b);
+        System.out.println("Third Number = " + c);
+        System.out.println("Average = " + average);
+    }
+}
