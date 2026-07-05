380500394f3bfc0986a67060c7946b95e723c9cc
Kalpana. M <kalpanaramakrishna25@gmail.com>
2026-07-05 09:57:16 +0530

    Create Division.java

---
+++ Division.java
@@ -0,0 +1,11 @@
+public class Division {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 5;
+        int quotient = a / b;
+
+        System.out.println("First Number: " + a);
+        System.out.println("Second Number: " + b);
+        System.out.println("Quotient = " + quotient);
+    }
+}
